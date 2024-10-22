1b1ed954d3943e9d767301c99772537a05f0b23f
VietBinh17 <154102208+binh12vv@users.noreply.github.com>
2024-10-22 23:47:03 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
-#include "uart.h"
+#include "my_stm32f4_gpio_lib.h"
 
-int main(){
+
+GPIO_InitTypeDef   myGPIO_InitStruct;
+int main(void) {
+     myGPIO_InitStruct.Mode =GPIO_MODE_OUTPUT_PP;
+     myGPIO_InitStruct.Pin  =GPIO_PIN_5;
+	   myGPIO_InitStruct.Pull =GPIO_NOPULL;
 	
-	USART2_Init();
-	
-	//test_setup();
-	printf("hello world");
-	
-	while(1){
-		
-	}
+	__LIB_RCC_GPIOA_CLK_ENABLE();
+	LIB_GPIO_Init(GPIOA, &myGPIO_InitStruct);
+    while (1) {
+        for (int i = 0; i < 900000; i++) {}  // Vòng lap tre
+      LIB_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
+		}
 }
